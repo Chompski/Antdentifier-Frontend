8932238715df4695372d94e050e94ed62b3ddbb6
chompski <tutters2@hotmail.com>
2018-07-20 18:09:02 +0100

    Reworked css styling using flex boxes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './ants.gif';
 import BG from './background.png';
-import './Body';
+import './App.css';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import NotFound from "./NotFound"
 import Body from "./Body"
@@ -11,19 +11,18 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <header>
         <img src={BG} id="bg" alt="" />
-          <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />
         </header>
-        <body className="App-Body">
-         
+        <main>  
         <BrowserRouter>
           <Switch>
             <Route exact path="/" component={Body}/>
             <Route component={NotFound} />
           </Switch>
         </BrowserRouter>
-        </body>
+        </main>
       </div>
     );
   }
